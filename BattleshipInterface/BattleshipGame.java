4f2495e6b8b4485b3205f746f9b54956f746fdd9
Jimmyswift <43080927+MillerHollinger@users.noreply.github.com>
2018-10-09 21:34:55 -0400

    Spacing and warning

--- BattleshipInterface/BattleshipGame.java
+++ BattleshipInterface/BattleshipGame.java
@@ -14,6 +14,10 @@ import java.util.*;
 
 public class BattleshipGame implements BattleshipInterface{
 
+	/* You won't need to edit anything inside the main aside the TODO requests.
+	 * In fact, if you do change anything, it probably won't work as intended.
+	 * Make sure you're editing the right code.
+	 */
 	public static void main(String[] args) {
 		// The scanner. Use it to get user input.
 		Scanner scan = new Scanner(System.in);
@@ -31,7 +35,8 @@ public class BattleshipGame implements BattleshipInterface{
 		
 		/* TODO Create some characters.
 		 * There should be five unique ship characters in this array.
-		 * Remember, your characters can't be " ", "X", or "O".
+		 * Re
+		 * member, your characters can't be " ", "X", or "O".
 		 */
 		// The ship's display characters. There should be five.
 		String[] shipChars = {"" , "", "", "", ""};
@@ -49,6 +54,7 @@ public class BattleshipGame implements BattleshipInterface{
 			// This is the main game loop. The player and AI alternate until somebody wins.
 			
 			/* TODO Tell the player it is their turn. */
+			
 			// Displays the player and AI's board (AI's is hidden).
 			System.out.println("Your board:");
 			game.printFullBoard(board);
@@ -56,19 +62,25 @@ public class BattleshipGame implements BattleshipInterface{
 			game.printHiddenBoard(ai, hiddenChar);
 			
 			/* TODO Ask the player which row they will shoot. */
+			
 			int tRow = Integer.parseInt(scan.nextLine()); // Gets the target row.
+			
 			/* TODO Ask the player with column they will shoot. */
+			
 			int tCol = Integer.parseInt(scan.nextLine()); // Gets the target column.
 			
 			// Fire the player's shot.
 			game.shootAt(ai, tRow, tCol);
+			
 			/* TODO Tell the player their shot was fired and it is now the AI's turn. */
+			
 			// The AI takes their turn.
 			game.takeAIMove(board);
 		}
 		if (game.checkVictory(board, shipChars)) // If the AI has won, this section will run.
 		{
 			/* TODO Demean the player for losing to the AI. Boo! */
+			
 			System.out.println("Your board: (LOSE)");
 			game.printFullBoard(board); // Shows the player's final board.
 			System.out.println("AI's board: (WIN)");
@@ -77,6 +89,7 @@ public class BattleshipGame implements BattleshipInterface{
 		else // If the player has won, this section will run.
 		{
 			/* TODO Congratulate the player for defeating the AI. Hooray! */
+			
 			System.out.println("Your board: (WIN)");
 			game.printFullBoard(board); // Show the player's final board.
 			System.out.println("AI's board: (LOSE)");
@@ -88,9 +101,6 @@ public class BattleshipGame implements BattleshipInterface{
 	 * Below are all the functions from BattleshipInterface.
 	 * Read each function's description in BattleshipInterface to see
 	 *  how to fill them in.
-	 *  
-	 * Your file will not compile until you complete each function.
-	 * 
 	 */
 	
 	/* TODO : Checks to see if there is a ship block somewhere.
