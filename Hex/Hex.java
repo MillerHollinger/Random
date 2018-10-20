a1dcf77c6e5c0a746eb15116ad110db1c330783c
Jimmyswift <43080927+MillerHollinger@users.noreply.github.com>
2018-10-19 22:25:53 -0400

    Typing functions

--- Hex/Hex.java
+++ Hex/Hex.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -74,17 +75,17 @@ public class Hex {
 		// Execute action
 	}
 
-	// Does autoclicking
+	// TODO Does autoclicking
 	public static void autoclick(String fileName) {
 
 	}
 
-	// Does alternating
+	// TODO Does alternating
 	public static void alternate(String fileName) {
 
 	}
 
-	// Does a macro
+	// TODO Does a macro
 	public static void macro(String fileName) {
 
 	}
@@ -98,21 +99,187 @@ public class Hex {
 	}
 
 	// Presses a key and holds for some time
-	public static void press(String key, int ms) {
-
+	public static void press(String key, int ms) throws Exception {
+		Robot bot = new Robot();
+		bot.setAutoDelay(ms);
+		switch (key) {
+		case "0":
+			bot.keyPress(KeyEvent.VK_0);
+			bot.keyRelease(KeyEvent.VK_0);
+			break;
+		case "1":
+			bot.keyPress(KeyEvent.VK_1);
+			bot.keyRelease(KeyEvent.VK_1);
+			break;
+		case "2":
+			bot.keyPress(KeyEvent.VK_2);
+			bot.keyRelease(KeyEvent.VK_2);
+			break;
+		case "3":
+			bot.keyPress(KeyEvent.VK_3);
+			bot.keyRelease(KeyEvent.VK_3);
+			break;
+		case "4":
+			bot.keyPress(KeyEvent.VK_4);
+			bot.keyRelease(KeyEvent.VK_4);
+			break;
+		case "5":
+			bot.keyPress(KeyEvent.VK_5);
+			bot.keyRelease(KeyEvent.VK_5);
+			break;
+		case "6":
+			bot.keyPress(KeyEvent.VK_6);
+			bot.keyRelease(KeyEvent.VK_6);
+			break;
+		case "7":
+			bot.keyPress(KeyEvent.VK_7);
+			bot.keyRelease(KeyEvent.VK_7);
+			break;
+		case "8":
+			bot.keyPress(KeyEvent.VK_8);
+			bot.keyRelease(KeyEvent.VK_8);
+			break;
+		case "9":
+			bot.keyPress(KeyEvent.VK_9);
+			bot.keyRelease(KeyEvent.VK_9);
+			break;
+		case "a":
+			bot.keyPress(KeyEvent.VK_A);
+			bot.keyRelease(KeyEvent.VK_A);
+			break;
+		case "b":
+			bot.keyPress(KeyEvent.VK_B);
+			bot.keyRelease(KeyEvent.VK_B);
+			break;
+		case "c":
+			bot.keyPress(KeyEvent.VK_C);
+			bot.keyRelease(KeyEvent.VK_C);
+			break;
+		case "d":
+			bot.keyPress(KeyEvent.VK_D);
+			bot.keyRelease(KeyEvent.VK_D);
+			break;
+		case "e":
+			bot.keyPress(KeyEvent.VK_E);
+			bot.keyRelease(KeyEvent.VK_E);
+			break;
+		case "f":
+			bot.keyPress(KeyEvent.VK_F);
+			bot.keyRelease(KeyEvent.VK_F);
+			break;
+		case "g":
+			bot.keyPress(KeyEvent.VK_G);
+			bot.keyRelease(KeyEvent.VK_G);
+			break;
+		case "h":
+			bot.keyPress(KeyEvent.VK_H);
+			bot.keyRelease(KeyEvent.VK_H);
+			break;
+		case "i":
+			bot.keyPress(KeyEvent.VK_I);
+			bot.keyRelease(KeyEvent.VK_I);
+			break;
+		case "j":
+			bot.keyPress(KeyEvent.VK_J);
+			bot.keyRelease(KeyEvent.VK_J);
+			break;
+		case "k":
+			bot.keyPress(KeyEvent.VK_K);
+			bot.keyRelease(KeyEvent.VK_K);
+			break;
+		case "l":
+			bot.keyPress(KeyEvent.VK_L);
+			bot.keyRelease(KeyEvent.VK_L);
+			break;
+		case "m":
+			bot.keyPress(KeyEvent.VK_M);
+			bot.keyRelease(KeyEvent.VK_M);
+			break;
+		case "n":
+			bot.keyPress(KeyEvent.VK_N);
+			bot.keyRelease(KeyEvent.VK_N);
+			break;
+		case "o":
+			bot.keyPress(KeyEvent.VK_O);
+			bot.keyRelease(KeyEvent.VK_O);
+			break;
+		case "p":
+			bot.keyPress(KeyEvent.VK_P);
+			bot.keyRelease(KeyEvent.VK_P);
+			break;
+		case "q":
+			bot.keyPress(KeyEvent.VK_Q);
+			bot.keyRelease(KeyEvent.VK_Q);
+			break;
+		case "r":
+			bot.keyPress(KeyEvent.VK_R);
+			bot.keyRelease(KeyEvent.VK_R);
+			break;
+		case "s":
+			bot.keyPress(KeyEvent.VK_S);
+			bot.keyRelease(KeyEvent.VK_S);
+			break;
+		case "t":
+			bot.keyPress(KeyEvent.VK_T);
+			bot.keyRelease(KeyEvent.VK_T);
+			break;
+		case "u":
+			bot.keyPress(KeyEvent.VK_U);
+			bot.keyRelease(KeyEvent.VK_U);
+			break;
+		case "v":
+			bot.keyPress(KeyEvent.VK_V);
+			bot.keyRelease(KeyEvent.VK_V);
+			break;
+		case "w":
+			bot.keyPress(KeyEvent.VK_W);
+			bot.keyRelease(KeyEvent.VK_W);
+			break;
+		case "x":
+			bot.keyPress(KeyEvent.VK_X);
+			bot.keyRelease(KeyEvent.VK_X);
+			break;
+		case "y":
+			bot.keyPress(KeyEvent.VK_Y);
+			bot.keyRelease(KeyEvent.VK_Y);
+			break;
+		case "z":
+			bot.keyPress(KeyEvent.VK_Z);
+			bot.keyRelease(KeyEvent.VK_Z);
+			break;
+		case "#": // Enter
+			bot.keyPress(KeyEvent.VK_ENTER);
+			bot.keyRelease(KeyEvent.VK_ENTER);
+			break;
+		case "/":
+			bot.keyPress(KeyEvent.VK_SLASH);
+			bot.keyRelease(KeyEvent.VK_SLASH);
+			break;
+		case ",":
+			bot.keyPress(KeyEvent.VK_COMMA);
+			bot.keyRelease(KeyEvent.VK_COMMA);
+			break;
+		case "+": // Shift Press
+			bot.keyPress(KeyEvent.VK_SHIFT);
+			break;
+		case "-": // Shift Release
+			bot.keyRelease(KeyEvent.VK_SHIFT);
+			break;
+		}
 	}
 
 	// Types multiple keys with a delay
-	public static void type(String key, int ms) {
-
+	public static void type(String keys, int ms) throws Exception {
+		for (int i = 0; i < keys.length(); i++)
+			press(keys, ms);
 	}
 
-	// File writer
+	// TODO File writer
 	public static void writeTo(String fileName, String toAdd) {
 
 	}
 
-	// File reader
+	// TODO File reader
 	public static String readFrom(String fileName) {
 		String out = "";
 
